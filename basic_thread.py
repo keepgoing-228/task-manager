7405929a88455e15911bc7523959cd7f3f44e6c3
keepgoing-228 <keepdling@gmail.com>
2025-05-29 12:32:35 +0800

    Add: basic thread function for lock

--- basic_thread.py
+++ basic_thread.py
@@ -3,35 +3,38 @@ import time
 
 
 def taskA(name):
-    for i in range(3):
-        print(f"[{name}] do {i+1} taskA")
+    lock.acquire()
+    for i in range(1, 6):
+        print(f"[{name}] do {i} taskA")
         time.sleep(0.5)
+        if i == 2:
+            lock.release()
 
 
 def taskB(name):
-    for i in range(3):
-        print(f"[{name}] do {i+1} taskB")
+    lock.acquire()
+    for i in range(1, 6):
+        print(f"[{name}] do {i*10} taskB")
         time.sleep(0.5)
+    lock.release()
 
 
 def taskC(name):
-    for i in range(3):
-        print(f"[{name}] do {i+1} taskC")
+    for i in range(1, 6):
+        print(f"[{name}] do {i*100} taskC")
         time.sleep(0.5)
 
 
+lock = threading.Lock()
 t1 = threading.Thread(target=taskA, args=("threadA",))
 t2 = threading.Thread(target=taskB, args=("threadB",))
 t3 = threading.Thread(target=taskC, args=("threadC",))
 t1.start()
 t2.start()
+# t3.start()
 
 # wait for the first two threads to finish
 t1.join()
 t2.join()
-
-# start the third thread after the first two threads finish
-t3.start()
-t3.join()
-
+# t3.join()
 print("all tasks done")
